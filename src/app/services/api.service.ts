1c8172d71ac42c62b4df1b6a604795e8de59bf7f
Alberto <al.zegers@duocuc.cl>
2023-10-27 14:11:52 -0300

    se agrego el servicio para el consumo de la API

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  url="https://fer-sepulveda.cl/API_PRUEBA_2/api-service.php"
+
+  constructor(private http: HttpClient) { }
+
+  almacenarPersona(usuario: string, correo: string, contrasena: string, nombre: string, apellido: string){
+    return this.http.post(this.url, {
+      nombreFuncion: 'UsuarioAlmacenar',
+      parametros: [
+        usuario, correo, contrasena, nombre, apellido
+      ]
+    }).pipe()
+  }
+  loginPersona(usario: string, contrasena: string){
+    return this.http.post(this.url, {
+      nombreFuncion: 'UsuarioLogin',
+      parametros: [
+        usario, contrasena
+      ]
+    }).pipe()
+  }
+  modificarPassPersona(usuario: string, contrasenaNueva: string, contrasenaActual: string){
+    return this.http.patch(this.url, {
+      nombreFuncion: 'UsuarioModificarContrasena',
+      parametros: [
+        usuario, contrasenaNueva, contrasenaActual
+      ]
+    }).pipe()
+  }
+}
